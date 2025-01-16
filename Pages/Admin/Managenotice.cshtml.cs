e39dc5a78001300de08edbcd9348ac5f49e5c645
npmzero <surajpathak3430@gmail.com>
2025-01-16 23:21:52 +0545

    added 4 admin routes and one user form fillup/register route

---
+++ Pages/Admin/Managenotice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wardalert.Pages.Admin
+{
+    public class ManagenoticeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Admin/Userlist.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wardalert.Pages.Admin
+{
+    public class UserlistModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
