c780ece02abc6582ec85aa397f8438e0c05c2447
npmzero <surajpathak3430@gmail.com>
2025-01-16 21:11:18 +0545

    Training DropDown Menu added

---
+++ Pages/Training/Upcoming.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace wardalert.Pages.Training
+{
+    public class UpcomingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
