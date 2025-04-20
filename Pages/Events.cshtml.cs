c58c0f00a2ce63101e538a487029d6d4d6daaf05
Sudipa amgai <sudipaamgai@gmail.com>
2025-04-20 22:41:51 +0545

    event model

--- Pages/Events.cshtml.cs
+++ Pages/Events.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace wardalert.Pages
 {
     public class EventsModel : PageModel
     {
+        private readonly string _connectionString = "Server=localhost;Database=project;User=root;Password=;AllowZeroDateTime=True;ConvertZeroDateTime=True;";
+        public List<EventCard> Events { get; set; } = new List<EventCard>();
+
         public void OnGet()
         {
+            using var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            string query = "SELECT  Title, Description FROM training WHERE Status = 'Ongoing'";
+            using var command = new MySqlCommand(query, connection);
+            using var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Events.Add(new EventCard
+                {
+                    id = reader.GetInt32("id"),
+                    Title = reader.GetString("Title"),
+                    Description = reader.GetString("Description")
+                });
+            }
+            connection.Close();
         }
+
+        public class EventCard
+        {
+            public int id { get; set; }
+            public string Title { get; set; }         
+            public string Description { get; set; }
+            public DateTime UploadedAt { get; set; }
+        }
+       
     }
 }
