c516ec02f03baeba5837617b2548a2ae7a8bd4b4
npmzero <surajpathak3430@gmail.com>
2025-04-22 00:37:36 +0545

    feat: Order events by UploadedAt in descending order - not proper

--- Pages/Events.cshtml.cs
+++ Pages/Events.cshtml.cs
@@ -14,7 +14,7 @@ namespace wardalert.Pages
             using var connection = new MySqlConnection(_connectionString);
             connection.Open();
 
-            string query = "SELECT  id , Title, Description , UploadedAt FROM event";
+            string query = "SELECT  id , Title, Description , UploadedAt FROM event ORDER BY UploadedAt DESC";
             using var command = new MySqlCommand(query, connection);
             using var reader = command.ExecuteReader();
 
